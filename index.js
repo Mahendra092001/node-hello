43972788f54b65e6ab74043dd8eeb55ce22555d6
Mahendra092001 <48065043+Mahendra092001@users.noreply.github.com>
2021-08-01 10:50:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const http = require('http');
 const port = process.env.PORT || 3000;
+fs = require('fs');
 
+fs.readFile('./Photography.html',function(err, html){
+  if(err){
+    throw err;
+  }
+  
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello Node!\n'
-  res.sendFile('Photography.html')
+  const msg = 'Hello Node!\n';
+  res.writeHeader(200,{"Content-Type":"text/html"});
+  res.write(html);
+  res.end(msg);
 });
 
 server.listen(port, () => {
